ba46e7ad1b4ab981a0a74e24f51268697acd1bb1
Mauricio <maugravena@gmail.com>
2019-12-23 16:21:40 -0300

    new implementation for wrapper component

--- app/src/pages/Dashboard/index.js
+++ app/src/pages/Dashboard/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useRouteMatch, Switch, Route } from 'react-router-dom';
-import DashboadWrapper from '../../components/DashboardWrapper';
 import HeaderDashboard from '../../components/HeaderDashboad';
 import NavbarDashboard from '../../components/NavbarDashboard';
 import Empty from '../Empty';
 import FAQ from '../FAQ';
 
-const Main = styled.main`
+const Wrapper = styled.main`
+  background-color: #fafafa;
   display: flex;
   flex-direction: column;
-  height: 100%;
+  height: 100vh;
+`;
+
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
   margin-top: 30px;
   padding: 0 25px;
 
@@ -24,16 +29,16 @@ function Dashboad() {
   const { path } = useRouteMatch();
 
   return (
-    <DashboadWrapper>
+    <Wrapper>
       <HeaderDashboard />
-      <Main>
+      <Section>
         <NavbarDashboard />
         <Switch>
           <Route path={`${path}/faq`} component={FAQ} />
           <Route component={Empty} />
         </Switch>
-      </Main>
-    </DashboadWrapper>
+      </Section>
+    </Wrapper>
   );
 }
 
